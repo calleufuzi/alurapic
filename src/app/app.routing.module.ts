ba60370931d822010d3830b93485930b4be59159
Calleu  Fuzi <calleufuzi@Calleus-MacBook-Pro.local>
2018-11-24 20:16:48 -0200

    created load button

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
+
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './erros/not-found/not-found.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-  {path: 'user/:userName', component: PhotoListComponent},
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver
+    }
+  },
   {path: 'p/add', component: PhotoFormComponent},
   {path: '**', component: NotFoundComponent},
 ]
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,30 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators'
+
 import { Photo } from '../photo/photo';
+import { Subject } from 'rxjs';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
 
   photos: Photo[]= [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private photoService:PhotoService,
-    private activatedRoute: ActivatedRoute
-  ){
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService){}
 
+  ngOnInit(){\
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter)
+  }
 
+  ngOnDestroy(){
+    this.debounce.unsubscribe();
   }
-  ngOnInit(){
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService.listFromUser(userName)
-    .subscribe(photos => this.photos = photos)
-  }
+  load() {
+    this.photoService
+        .listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+            this.photos = this.photos.concat(photos);
+            if(!photos.length) this.hasMore = false;
+        });
+}
 
 }
