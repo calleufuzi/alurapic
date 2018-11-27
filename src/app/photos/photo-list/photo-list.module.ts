9d4f7f9c5cde73bf4325588289627730d56bfb55
Calleu  Fuzi <calleufuzi@Calleus-MacBook-Pro.local>
2018-11-27 10:30:30 -0200

    created card component

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescription } from './filter-by-description.pipe';
-import { CommonModule } from '@angular/common';
 import { PhotoModule } from '../photo/photo.module';
+import { CardComponent } from 'src/app/shared/components/card/card.component';
+import { CardModule } from 'src/app/shared/components/card/card.module';
 
 @NgModule({
   declarations:[
@@ -15,7 +18,8 @@ import { PhotoModule } from '../photo/photo.module';
   ],
   imports:[
       CommonModule,
-      PhotoModule
+      PhotoModule,
+      CardModule,
   ]
 })
 
